dd71567acb2938d7bc8055d8fdd57226be2563af
Miancho <miancho@gmail.com>
2020-08-14 13:11:27 -0300

    primer commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <cld-image
+        public-id="examples/avatar"
+        width="900"
+        height="900"
+        crop="thumb"
+        fetchFormat="auto"
+        quality="auto"
+        responsive
+      />
+    </div>
+    <div>
+      <cld-image public-id="examples/avatar">
+        <cld-transformation width="100" height="100" crop="thumb" />
+        <cld-transformation fetchFormat="auto" quality="auto" />
+      </cld-image>
+    </div>
+  </div>
+</template>
